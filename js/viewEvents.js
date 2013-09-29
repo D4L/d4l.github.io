2f9a9bcf2dfe3a7ef2b02f02e100e5d0ca201188
D4L <Austin.L.D4L@gmail.com>
2013-09-29 19:14:49 -0400

    Updated built site from develop - e4a425a4de4a2491046b537a715381606d0f11f4

--- js/viewEvents.js
+++ js/viewEvents.js
@@ -1,10 +1,13 @@
-$(document).ready( function() {
-  $("#scroll-spy").on('activate.bs.scrollspy', function(e) {
-    $(".jumbotron").animate({
-      backgroundColor: "#eee"
-    }, 200);
-    $($(e.target).data("activates")).animate( {
-      backgroundColor: "#f671bb"
-    }, 200);
+(function() {
+  $(function() {
+    return $("#scroll-spy").on('activate.bs.scrollspy', function(e) {
+      $(".jumbotron").animate({
+        backgroundColor: "#eee"
+      }, 200);
+      return $($(e.target).data("activates")).animate({
+        backgroundColor: "#f671bb"
+      }, 200);
+    });
   });
-});
+
+}).call(this);
